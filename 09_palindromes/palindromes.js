dc6556abe82b20f079f8bc8f024a3578cd64e6ab
ChooJunIan <ianchoo2000@gmail.com>
2025-03-25 16:06:28 +0800

    Complete all javascript exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+  let splitArr = sentence.split('').filter((char) => {
+    return /[a-zA-Z0-9]/.test(char);
+  }).map(word => word.toLowerCase());
 
+  let splitArrOri = splitArr.slice().join("");
+  let rejoined = splitArr.reverse().join("");
+  console.log(splitArrOri);
+
+  return splitArrOri === rejoined;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let intNum = parseInt(num); 
+  if (intNum == 0) {
+    return 0;
+  }
+  
+  if (intNum < 0) {
+    return "OOPS";
+  }
 
+  let firstPrev = 1;
+  let secondPrev = 0; 
+
+  for (let i = 2; i <= intNum ; i++) {
+    currentNum = firstPrev + secondPrev
+    secondPrev = firstPrev;
+    firstPrev = currentNum;
+  }
+
+  return firstPrev;
+  
 };
 
 // Do not edit below this line
